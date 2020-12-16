54f52c1b702d7ac4233f669a8c0f3521bf414d7f
Chris Chandler <chris.chandler@beam.dental>
2020-12-16 08:14:46 -0500

    install gh and move aliases to topic folder

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,3 +22,7 @@
 # alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 # alias gac='git add -A && git commit -m'
 # alias ge='git-edit-new'
+
+alias ghweb="gh pr view --web"
+alias prweb="gh pr view --web"
+alias pr="gh pr view --web"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,3 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias be="bundle exec"
-alias ghweb="gh pr view --web"
-alias prweb="gh pr view --web"
-alias pr="gh pr view --web"
