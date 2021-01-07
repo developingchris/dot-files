9597ea59ce20c80407e733d47a25445c8dd389d9
Chris Chandler <chris.chandler@beam.dental>
2021-01-07 09:55:02 -0500

    add fzf as a top level topic folder

---
+++ fzf/fzf.zsh
@@ -0,0 +1,7 @@
+if type ag &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='ag -p ~/.fzfignore -g ""'
+fi
+
+# if type gg &> /dev/null; then
+#     export FZF_DEFAULT_COMMAND='ag -p ~/.fzfignore -g ""'
+# fi
