929858f0d0fc12f771863314628834c0d75a9f01
Chris Chandler <chris.chandler@beam.dental>
2022-03-15 14:37:14 -0400

    core changes for updates

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,5 +1,5 @@
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PYENV_ROOT/shims:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
