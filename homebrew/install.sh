512e47514928671ce15d8ee7f1142ecedfb3d173
Chris Chandler <chris.chandler@reportallusa.com>
2024-11-04 14:13:55 -0500

    homebrew install working

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
+echo "  Installing Homebrew for you."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-fi
+brew bundle
 
 exit 0
