db5d746f92ff8d84d2303a9ff8470ab6a3cddfbc
Chris Chandler <chris.chandler@reportallusa.com>
2024-11-04 14:09:49 -0500

    add oh-my-zsh to the install process

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,1 @@
-
-
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "ZDOTDIR=~/.dotfiles/.oh-my-zsh $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
